7b06d671f3b570f94290579ad8f73333eab7b585
xrelkd <46590321+xrelkd@users.noreply.github.com>
2023-11-13 16:01:41 +0800

    feat: update `block length` to 30MiB and `reference time` to 2secs

--- runtime/general/src/lib.rs
+++ runtime/general/src/lib.rs
@@ -153,7 +153,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("thxnet-general-runtime"),
     impl_name: create_runtime_str!("thxnet-general-runtime"),
     authoring_version: 1,
-    spec_version: 4,
+    spec_version: 6,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
@@ -168,11 +168,11 @@ const AVERAGE_ON_INITIALIZE_RATIO: Perbill = Perbill::from_percent(5);
 /// used by `Operational` extrinsics.
 const NORMAL_DISPATCH_RATIO: Perbill = Perbill::from_percent(75);
 
-/// We allow for 2.5 of a second of compute with a 12 second average block time.
+/// We allow for 0.5 of a second of compute with a 12 second average block time.
 /// We fix proof size at 30MiB.
 const MAXIMUM_BLOCK_WEIGHT: Weight = Weight::from_parts(
-    WEIGHT_REF_TIME_PER_SECOND.saturating_mul(5).saturating_div(2),
-    60 * cumulus_primitives_core::relay_chain::MAX_POV_SIZE as u64,
+    WEIGHT_REF_TIME_PER_SECOND.saturating_div(2),
+    6 * cumulus_primitives_core::relay_chain::MAX_POV_SIZE as u64,
 );
 
 /// The version information used to identify this runtime when compiled
