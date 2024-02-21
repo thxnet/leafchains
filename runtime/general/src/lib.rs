1df6b9f66dfb85669d008b46364d68794d4e1420
xrelkd <46590321+xrelkd@users.noreply.github.com>
2024-02-21 15:10:03 +0800

    feat(runtime/general): bump runtime `spec_version` to 3

--- runtime/general/src/lib.rs
+++ runtime/general/src/lib.rs
@@ -153,7 +153,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("thxnet-general-runtime"),
     impl_name: create_runtime_str!("thxnet-general-runtime"),
     authoring_version: 1,
-    spec_version: 2,
+    spec_version: 3,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
